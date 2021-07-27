f249f7a354604199f2ecda0243b97adf483458c3
Henok Gashaw <65940650+Henok100@users.noreply.github.com>
2021-07-27 11:21:05 +0300

    Update string_helpers.c

--- string_helpers.c
+++ string_helpers.c
@@ -1,31 +1,97 @@
 #include "holberton.h"
 
 /**
- * reverse_string - reverses a string in place
+ * print_u - prints an unsigned in in decimal notation
+ * @u: unsigned int to print
  *
- * @s: string to reverse
- * Return: A pointer to a character
+ * Return: number of digits printed
  */
-char *reverse_string(char *s)
+int print_u(va_list u)
 {
-	int len, head;
-	char tmp;
-	char *dest;
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
 
-	for (len = 0; s[len] != '\0'; len++)
-		dest = malloc(sizeof(char) * len + 1);
-	if (dest == NULL)
-		return (NULL);
-	_memcpy(dest, s, len);
-	for (head = 0; head < len; head++, len--)
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
 	{
-		tmp = dest[len - 1];
-		dest[len - 1] = dest[head];
-		dest[head] = tmp;
+		m /= 10;
+		a[i] = (n / m) % 10;
 	}
-	return (dest);
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
 
+/**
+ * print_o - takes an unsigned int and prints it in octal notation
+ * @o: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_o(va_list o)
+{
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
 
+	n = va_arg(o, unsigned int);
+	m = 1073741824; /* (8 ^ 10) */
+	a[0] = n / m;
+	for (i = 1; i < 11; i++)
+	{
+		m /= 8;
+		a[i] = (n / m) % 8;
+	}
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
 
+/**
+ * print_b - takes an unsigned int and prints it in binary notation
+ * @b: unsigned in to print
+ *
+ * Return: number of digits printed
+ */
+int print_b(va_list b)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
+	n = va_arg(b, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
